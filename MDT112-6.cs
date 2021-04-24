90994993d851bdcf28d0de8d71435fea2c48c0f2
zinxtoz <49581160+zinxtoz@users.noreply.github.com>
2021-04-24 16:11:25 +0700

    Add files via upload

---
+++ MDT112-6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alp = Console.ReadLine();
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+
+            for( int y = w; y <n; y++)
+            {
+                for (int x=0; x<w;x++)
+                    if (x >= y)
+                    {
+                        Console.WriteLine(alp);
+                    }else   {
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+}
