1756ffe07c6bdae7246f084f74cd694999d310a9
zinxtoz <49581160+zinxtoz@users.noreply.github.com>
2021-04-24 14:25:11 +0700

    Add files via upload

---
+++ MDT112-4.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y, dx, dy, step;
+            float x1 = float.Parse(Console.ReadLine());
+            float y1 = float.Parse(Console.ReadLine());
+            float x2 = float.Parse(Console.ReadLine());
+            float y2 = float.Parse(Console.ReadLine());
+
+            dx = x2 - x1;
+            dy = y2 - y1;
+            if (Math.Abs(dx) >= Math.Abs(dy))
+            {
+                step = Math.Abs(dx);
+            }
+            else
+            {
+                step = Math.Abs(dy);
+            }
+            dx /= step;
+            dy /= step;
+            x = x1;
+            y = y1;
+            int i = 1;
+            while (i <= step)
+            {
+                Console.WriteLine("{0},{1}", x, y);
+                x += dx;
+                y += dy;
+                i++;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
